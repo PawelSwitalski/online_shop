ad92179e6da79491e17bea12cd0ea17334330b2e
PawelSwitalski <paswitalski@gmail.com>
2023-11-09 15:25:38 +0000

    Changed vite file and package.json

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+
+    server: { 
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: [
+		'resources/js/bootstrap.js',
+		'resources/css/app.css',
                 'resources/sass/app.scss',
                 'resources/js/app.js',
             ],
