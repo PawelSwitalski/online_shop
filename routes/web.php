0fe06814f87d3556a92d5414d489b1808f4ff45f
PawelSwitalski <paswitalski@gmail.com>
2023-11-09 13:22:04 +0000

    Added laravel/ui and bootstrap

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', [HelloWorld::class, 'show']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
